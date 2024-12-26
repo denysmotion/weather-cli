bcc6be01a608281033771ed82160e8e001d43352
denysmotion <denysmotion@gmail.com>
2024-12-26 15:26:44 +0100

    nice output of weather in the console

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { getWeather } from './services/api.service.js';
-import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue } from './services/storage.service.js';
 
 const saveToken = async (token) => {
     if (!token.length) {
@@ -17,21 +17,49 @@ const saveToken = async (token) => {
     }
 }
 
+const saveCity = async (city) => {
+    if (!city.length) {
+        printError('No city');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess('City saved');
+    } catch (e) {
+        printError(e.message);
+    }
+}
+
+const getForcast = async () => {
+    try {
+        const weather = await getWeather(await getKeyValue(TOKEN_DICTIONARY.city));
+        printWeather(weather, getIcon(weather.weather[0].icon));
+    } catch (e) {
+        if (e?.response?.status == 404) {
+            printError('Incorrect city name');
+        } else if (e?.response?.status == 404) {
+            printError('Invalid token');
+        } else {
+            printError(e.message);
+        }
+    }
+}
+
 const initCLI = () => {
     const args = getArgs(process.argv);
 
     if (args.h) {
-        printHelp();
+        return printHelp();
     }
 
     if (args.s) {
-
+        return saveCity(args.s);
     }
 
     if (args.t) {
         return saveToken(args.t);
     }
-    getWeather('szczecin');
+    return getForcast();
 }
 
 initCLI();
