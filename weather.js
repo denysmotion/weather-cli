06a9c3dd4db22a7f17b2bf3f8e9930ff3c95bc74
denysmotion <denysmotion@gmail.com>
2024-12-26 14:27:49 +0100

    add api

--- weather.js
+++ weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+    if (!token.length) {
+        printError('No token');
+        return;
+    }
     try {
-        await saveKeyValue('token', token);
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
         printSuccess('Token saved');
     } catch (e) {
         printError(e.message);
@@ -14,7 +19,7 @@ const saveToken = async (token) => {
 
 const initCLI = () => {
     const args = getArgs(process.argv);
-    
+
     if (args.h) {
         printHelp();
     }
@@ -26,6 +31,7 @@ const initCLI = () => {
     if (args.t) {
         return saveToken(args.t);
     }
+    getWeather('szczecin');
 }
 
 initCLI();
